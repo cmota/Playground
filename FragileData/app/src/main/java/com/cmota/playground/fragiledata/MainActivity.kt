fc287f0ffc92d8e6a3a1381eb1b6b24893d01b3c
Carlos Mota <cafonsomota@gmail.com>
2020-07-20 12:24:03 +0100

    Added hasFragileUserData blog post sample

---
+++ FragileData/app/src/main/java/com/cmota/playground/fragiledata/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cmota.playground.fragiledata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
